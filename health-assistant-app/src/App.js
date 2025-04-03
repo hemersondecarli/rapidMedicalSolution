1677404c07a4b3c334117748055c946a4e2b56e4
Hemerson de Carli <hemersondc96@gmail.com>
2025-04-03 04:52:19 +0100

    - Added route for Medical History page

--- health-assistant-app/src/App.js
+++ health-assistant-app/src/App.js
@@ -5,13 +5,14 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import MoodSelection from './pages/MoodSelection';
 import Diagnosis from './pages/Diagnosis';
-import PlaceOrders from './pages/PlaceOrders';
+// import PlaceOrders from './pages/PlaceOrders';
 import Medications from './pages/Medications';
 import MenstrualTracker from './pages/MenstrualTracker';
 import MentalSupport from './pages/MentalSupport';
 import AIDiagnosis from './pages/AIDiagnosis';
 import Registration from './pages/Registration';
 import MoodAnalytics from './pages/MoodAnalytics';
+import MedicalHistory from "./components/MedicalHistory";
 
 
 function App() {
@@ -23,13 +24,14 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/mood-selection" element={<MoodSelection />} />
         <Route path="/diagnosis" element={<Diagnosis />} />
-        <Route path="/place-orders" element={<PlaceOrders />} />
+        {/* <Route path="/place-orders" element={<PlaceOrders />} /> */}
         <Route path="/medications" element={<Medications />} />
         <Route path="/menstrual-tracker" element={<MenstrualTracker />} />
         <Route path="/mental-support" element={<MentalSupport />} />
         <Route path="/ai-diagnosis" element={<AIDiagnosis />} />
         <Route path="/Registration" element={<Registration />} />
         <Route path="/mood-analytics" element={<MoodAnalytics />} />
+        <Route path="/medical-history" element={<MedicalHistory />} />
       </Routes>
     </Router>
   );
